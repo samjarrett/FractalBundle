046cdf9c0ce6ba83cb7b58b7206811b6060f7bde
Sam Jarrett <samstah@gmail.com>
2015-08-16 19:08:31 +1000

    [CS] PSR-2 compliance

--- src/Manager.php
+++ src/Manager.php
@@ -7,44 +7,45 @@ use League\Fractal\Resource\ResourceAbstract;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class Manager extends BaseManager implements ContainerAwareInterface {
-	private $container;
+class Manager extends BaseManager implements ContainerAwareInterface
+{
+    private $container;
 
-	/**
-	 * Sets the Container.
-	 *
-	 * @param ContainerInterface|null $container A ContainerInterface instance or null
-	 */
-	public function setContainer(ContainerInterface $container = null)
-	{
-		$this->container = $container;
-	}
+    /**
+     * Sets the Container.
+     *
+     * @param ContainerInterface|null $container A ContainerInterface instance or null
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
 
-	/**
-	 * Create Data
-	 *
-	 * Main method to kick this all off. Make a resource then pass it over, and use toArray()
-	 *
-	 * @param \League\Fractal\Resource\ResourceAbstract $resource
-	 * @param string $scopeIdentifier
-	 * @param string $parentScopeInstance
-	 * @return \League\Fractal\Scope
-	 **/
-	public function createData(ResourceAbstract $resource, $scopeIdentifier = null, $parentScopeInstance = null)
-	{
-		$scopeInstance = new Scope($this, $resource, $scopeIdentifier);
-		$scopeInstance->setContainer($this->container);
+    /**
+     * Create Data
+     *
+     * Main method to kick this all off. Make a resource then pass it over, and use toArray()
+     *
+     * @param \League\Fractal\Resource\ResourceAbstract $resource
+     * @param string $scopeIdentifier
+     * @param string $parentScopeInstance
+     * @return \League\Fractal\Scope
+     **/
+    public function createData(ResourceAbstract $resource, $scopeIdentifier = null, $parentScopeInstance = null)
+    {
+        $scopeInstance = new Scope($this, $resource, $scopeIdentifier);
+        $scopeInstance->setContainer($this->container);
 
-		// Update scope history
-		if ($parentScopeInstance !== null) {
+        // Update scope history
+        if ($parentScopeInstance !== null) {
 
-			// This will be the new children list of parents (parents parents, plus the parent)
-			$scopeArray = $parentScopeInstance->getParentScopes();
-			$scopeArray[] = $parentScopeInstance->getCurrentScope();
+            // This will be the new children list of parents (parents parents, plus the parent)
+            $scopeArray = $parentScopeInstance->getParentScopes();
+            $scopeArray[] = $parentScopeInstance->getCurrentScope();
 
-			$scopeInstance->setParentScopes($scopeArray);
-		}
+            $scopeInstance->setParentScopes($scopeArray);
+        }
 
-		return $scopeInstance;
-	}
-} 
+        return $scopeInstance;
+    }
+}
--- src/Scope.php
+++ src/Scope.php
@@ -6,39 +6,40 @@ use League\Fractal\Scope as BaseScope;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-class Scope extends BaseScope implements ContainerAwareInterface {
-	/**
-	 * @var ContainerInterface
-	 */
-	private $container;
+class Scope extends BaseScope implements ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
 
-	/**
-	 * Sets the Container.
-	 *
-	 * @param ContainerInterface|null $container A ContainerInterface instance or null
-	 */
-	public function setContainer(ContainerInterface $container = null)
-	{
-		$this->container = $container;
-	}
+    /**
+     * Sets the Container.
+     *
+     * @param ContainerInterface|null $container A ContainerInterface instance or null
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
 
-	/**
-	 * Fire the main transformer.
-	 *
-	 * @internal
-	 * @param  callable|\League\Fractal\TransformerAbstract  $transformer
-	 * @param  mixed  $data
-	 * @return array
-	 */
-	protected function fireTransformer($transformer, $data)
-	{
-		if (is_string($transformer) && $this->container->has($transformer)) {
-			$transformer = $this->container->get($transformer);
-			if ($transformer instanceof ContainerAwareInterface) {
-				$transformer->setContainer($this->container);
-			}
-		}
+    /**
+     * Fire the main transformer.
+     *
+     * @internal
+     * @param  callable|\League\Fractal\TransformerAbstract  $transformer
+     * @param  mixed  $data
+     * @return array
+     */
+    protected function fireTransformer($transformer, $data)
+    {
+        if (is_string($transformer) && $this->container->has($transformer)) {
+            $transformer = $this->container->get($transformer);
+            if ($transformer instanceof ContainerAwareInterface) {
+                $transformer->setContainer($this->container);
+            }
+        }
 
-		return parent::fireTransformer($transformer, $data);
-	}
+        return parent::fireTransformer($transformer, $data);
+    }
 }
